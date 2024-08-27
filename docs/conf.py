302f41936db91226438c8c97b5640ce572e1026e
Liam Olivier <135514909+Lincalibur@users.noreply.github.com>
2024-08-27 18:45:37 +0200

    Setup of Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,28 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../src'))
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Python-Setup-template'
+copyright = '2024, Liam Olivier'
+author = 'Liam Olivier'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
